21914cc1ae971db00027001a542c6869cf71e9fa
Akshit Thakur <akshit.235@gmail.com>
2020-09-05 21:04:20 +0530

    LinkedList setup

---
+++ LinkedList.java
@@ -0,0 +1,14 @@
+/**
+ * LinkedList
+ */
+public class LinkedList {
+    static class Node {
+        int data;
+        Node next;
+
+        public Node(int d) {
+            this.data = d;
+            this.next = null;
+        }
+    }
+}
